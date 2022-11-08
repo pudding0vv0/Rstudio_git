0f074fcaaf6bc48918e524fa69677032dab07112
xiaolong <2101336202@qq.com>
2022-11-09 03:11:43 +0800

    take a break

---
+++ build______git.R
@@ -0,0 +1,32 @@
+###########  安装更新git ##############
+# $ git clone git://git.kernel.org/pub/scm/git/git.git
+
+#重点章节：2
+
+
+git config --global --list  自己设置的本地配置
+
+git config --system --list  系统设定的配置
+所有的配置文件其实都保存在本地
+
+git clone # 从源上复制的连接 # 从远程拷贝到本地
+
+git status # 查看当前工作目录下的文件状态（是否commit等）
+
+git add . #添加到commit（放到暂存区）# 有空格
+
+tinytex::install_tinytex()
+
+
+
+
+#########连接github与Rstudio#############
+
+1 #先commit（贮藏到本地库）后push（绿色小箭头）
+2修改后的文件，点击保存后，RU（右上方的界面）的文件提示会变为蓝色方块M（表示文件有修改），
+修改后再commit然后push文件就更新了
+PS：没网的时候修改完先commit（贮存到当地库），网好后上传
+4 git_vaccinate() 简化commit and push，帮助一键上传
+5 over
+
+
